c5ddcab5b386de12f3fa308d35b11d052e16466c
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-11-30 15:40:49 +0000

    update: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { invoke } from '@tauri-apps/api/core'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.css'
 
 function App() {
