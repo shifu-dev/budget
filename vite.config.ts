c9e4d1c7b10abd2f98b1f5504bdc99aba04e7750
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-11-30 15:57:43 +0000

    update: add react-router

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default {
     alias: {
       '@app': path.resolve(__dirname, 'src/App'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@themes': path.resolve(__dirname, 'src/themes'),
     }
