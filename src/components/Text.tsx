05f195506a92e07abb82d197e749c67a18e05da8
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-11-30 19:16:24 +0000

    update: impl category in text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,3 +1,7 @@
+import { useTheme } from '@themes/index'
+import { Theme } from '@themes/Theme'
+import { CSSProperties } from 'react'
+
 export type TextValue = string | number | Date
 
 export type TextCategories = 'text' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
@@ -8,9 +12,34 @@ export interface TextProps {
 }
 
 export function Text(props: TextProps) {
-  const text = toText(props.value)
+  const defaultCategory: TextCategories = 'text'
 
-  return <>{text}</>
+  const theme = useTheme()
+  const text = toText(props.value)
+  const style = getStyle(theme, props.category ?? defaultCategory)
+
+  return <span style={style}>{text}</span>
+}
+
+const getStyle = (theme: Theme, category: TextCategories): CSSProperties => {
+  switch (category) {
+    case 'text':
+      return { fontSize: theme.textSize, color: theme.textColor }
+    case 'h1':
+      return { fontSize: theme.h1Size, color: theme.h1Color }
+    case 'h2':
+      return { fontSize: theme.h2Size, color: theme.h2Color }
+    case 'h3':
+      return { fontSize: theme.h3Size, color: theme.h3Color }
+    case 'h4':
+      return { fontSize: theme.h4Size, color: theme.h4Color }
+    case 'h5':
+      return { fontSize: theme.h5Size, color: theme.h5Color }
+    case 'h6':
+      return { fontSize: theme.h6Size, color: theme.h6Color }
+    default:
+      return {}
+  }
 }
 
 const toText = (value: TextValue): string => {
