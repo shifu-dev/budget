d9431ff0282a1632d43b7763b4264d78c4713a72
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-12-06 18:23:05 +0530

    update: fix button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { Icon, IconName } from '@components/Icon'
+import { useTheme } from '@themes/index'
 
 export type ButtonCallback = () => void
 
@@ -9,12 +10,17 @@ export interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
+  const theme = useTheme()
+
   return (
     <>
       <button
         title={props.label}
         style={{
+          borderWidth: 0,
           borderRadius: 50,
+          backgroundColor: theme.cardColor,
+          aspectRatio: 1,
         }}
       >
         <Icon name={props.icon} />
