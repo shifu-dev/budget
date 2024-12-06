c89c94a6207fa10449b7034f87b964cbf3ddc3ac
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-12-06 18:21:11 +0530

    update: add cost card component

---
+++ src/components/CostCard.tsx
@@ -0,0 +1,16 @@
+import { Text } from '@components/Text'
+import { Card, CardProps } from '@components/Card'
+
+export interface CostCardProps extends CardProps {
+  value: number
+}
+
+export function CostCard(props: CostCardProps) {
+  const costString = props.value.toString()
+
+  return (
+    <Card leftIcon='currency-rupee'>
+      {<Text value={costString} category='h4' />}
+    </Card>
+  )
+}
