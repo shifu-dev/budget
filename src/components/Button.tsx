881a0213388fcbcb0f5d9c9911a1bb2ed0b666f9
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-12-23 15:05:57 +0530

    update: add radio group and button size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,51 @@
 import { Text } from '@components/Text'
 import { Icon, IconName } from '@components/Icon'
 import { useTheme } from '@themes/index'
+import { CSSProperties } from 'react'
 
 export type ButtonCallback = () => void
+
+export type ButtonSize = 'sm' | 'md' | 'lg' | 'xl' | 'xxl'
 
 export interface ButtonProps {
   label?: string
   icon?: IconName
+  size?: ButtonSize
   onPress?: ButtonCallback
+  style?: CSSProperties
 }
 
 export function Button(props: ButtonProps) {
-  const theme = useTheme()
+  const style = _getStyle(props)
 
   return (
-    <div
-      onClick={props.onPress}
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
-        borderWidth: 0,
-        borderRadius: 50,
-        backgroundColor: theme.cardColor,
-        aspectRatio: 1,
-      }}
-    >
-      <Icon name={props.icon} />
+    <div onClick={props.onPress} style={style}>
+      <Icon name={props.icon} size='md' />
       <Text value={props.label} category='h5' />
     </div>
   )
+}
+
+const _getStyle = (props: ButtonProps): CSSProperties => {
+  const theme = useTheme()
+
+  let style: CSSProperties = {
+    minHeight: 70,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0,
+    borderRadius: 25,
+    backgroundColor: theme.cardColor,
+  }
+
+  if (props.size == 'sm') {
+    style.minWidth = 70
+    style.borderRadius = '50%'
+  } else {
+    style.minWidth = 200
+  }
+
+  return { ...style, ...props.style }
 }
