1f2d35f0151317669f881719bf9cfd606119e315
shifu-dev <csingh8476@gmail.com>
2025-01-11 08:17:54 +0000

    update: add labels in time wheel

--- src/components/TimeWheel.tsx
+++ src/components/TimeWheel.tsx
@@ -1,4 +1,5 @@
 import { NumWheel } from '@components/NumWheel'
+import { Text } from '@components/Text'
 
 export interface TimeWheelProps {
   hour?: number
@@ -31,24 +32,68 @@ export const TimeWheel = (props: TimeWheelProps) => {
   const showMinute = props.showMinutes ?? defaults.showMinute
   const showSecond = props.showSeconds ?? defaults.showSecond
 
+  function CustomWheel(args: {
+    show: boolean
+    title: string
+    value: any
+    to: number
+    onSelect: any
+  }) {
+    const { show, title, value, to, onSelect } = args
+
+    if (!show) return
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          gap: 10,
+        }}
+      >
+        <Text value={title} category='h5' />
+        <NumWheel
+          value={value}
+          to={to}
+          onSelect={onSelect}
+          style={{
+            height: 200,
+          }}
+        />
+      </div>
+    )
+  }
+
   return (
     <div
       style={{
         display: 'flex',
-        flexDirection: 'row',
-        height: '100%',
-        width: '100%',
+        justifyContent: 'space-around',
       }}
     >
-      {showHour && (
-        <NumWheel value={hour} to={23} onSelect={props.onHourSelect} />
-      )}
-      {showMinute && (
-        <NumWheel value={minute} to={59} onSelect={props.onMinuteSelect} />
-      )}
-      {showSecond && (
-        <NumWheel value={second} to={59} onSelect={props.onSecondSelect} />
-      )}
+      <CustomWheel
+        title='Hour'
+        show={showHour}
+        value={hour}
+        to={23}
+        onSelect={props.onHourSelect}
+      />
+      <CustomWheel
+        title='Minute'
+        show={showMinute}
+        value={minute}
+        to={59}
+        onSelect={props.onMinuteSelect}
+      />
+      <CustomWheel
+        title='Second'
+        show={showSecond}
+        value={second}
+        to={59}
+        onSelect={props.onSecondSelect}
+      />
     </div>
   )
 }
