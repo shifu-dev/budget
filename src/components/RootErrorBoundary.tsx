498771ea9eee107b885148bab2220e8369f897af
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-02-03 18:07:28 +0530

    update: improve error boundary view

--- src/components/RootErrorBoundary.tsx
+++ src/components/RootErrorBoundary.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 import { Text } from '@components/Text'
 import { Button } from '@components/Button'
+import { useTheme } from '@themes/index'
 
 export interface RootErrorBoundaryProps {
   children?: ReactNode
@@ -13,22 +14,39 @@ export function RootErrorBoundary(props: RootErrorBoundaryProps) {
   }
 
   function FallbackComponent(props: FallbackProps) {
+    const theme = useTheme()
+
     return (
       <div
         style={{
-          height: '100%',
+          height: '100vh',
+          width: '100vw',
+          overflow: 'hidden',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           flexDirection: 'column',
-          padding: 40,
-          gap: 40,
-          backgroundColor: 'red',
+          backgroundColor: theme.colors.background,
         }}
       >
-        <Text value='An unknown error occurred. Click the button below to reset.' />
-        <Text value={props.error} />
-        <Button label='Reload' onPress={onReload} />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            padding: 50,
+            gap: 30,
+          }}
+        >
+          <Text
+            value='An unknown error occurred. Click the button below to reset.'
+            category='h4'
+            align='center'
+          />
+          <Text value={props.error} />
+          <Button label='Reload' onPress={onReload} />
+        </div>
       </div>
     )
   }
